56aa5fb23e9fc34317813805bc46000720aa2913
Martin <mdsugarev@gmail.com>
2022-01-06 23:15:50 +0200

    e

--- src/js/Application.js
+++ src/js/Application.js
@@ -44,7 +44,8 @@ export default class Application extends EventEmitter {
 
   }
   _create(){
-    this._beat.on(Beat.events.BIT, function(){
+    const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
+    this._beat.addListener(Beat.events.BIT, function(){
       for(let i = 0; i < lyrics.length; i++){
   
         const message = document.createElement("div");
